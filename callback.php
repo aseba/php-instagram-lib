a05c85f93553358e3b59c5008091d62bd43f0865
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-25 14:41:04 -0300

    Posted data

--- callback.php
+++ callback.php
@@ -3,8 +3,8 @@
 if(isset($_GET['hub_challenge'])) echo $_GET['hub_challenge'];
 if(isset($_POST)){
 	$file = file_get_contents('/tmp/updates.instagram');
-	$data = print_r($_REQUEST);
-	$fulldata = $file . "\n\n" . $data;
+	$posted_data = file_get_contents("php://input");
+	$fulldata = $file . "\n\n" . $posted_data;
 	file_put_contents('/tmp/updates.instagram', $fulldata);
 	echo $file;
 }
