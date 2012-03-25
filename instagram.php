24f4b71ebc450869e1efa55e9e07d6b5b58b3bab
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-24 21:20:12 -0300

    Rebuilding

--- instagram.php
+++ instagram.php
@@ -19,14 +19,14 @@ class InstagramRealTime {
 			'aspect' => 'media'
 		));
 
-		var_dump(HttpClient::quickPost('https://api.instagram.com/v1/subscriptions/', $params));
+		$client = new HttpClient('https://api.instagram.com/v1/subscriptions/');
+		$client->setDebug(true);
+		$client->post('https://api.instagram.com/v1/subscriptions/', $params);
 	}
 }
 
 $irt = new InstagramRealTime('699495b3bfaf4632bdc5096e7544ff23', '68af47b6e9174f5dbb94ef913fdc42b3', 'http://public.olapic.com/~aseba/PHPIRT/instagram.php');
 
-if(isset($_GET)){
-
-}
+$irt->addSubscription();
 
 ?>
