f969833c04b4403d80b896bb11f6544185c72cef
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-25 15:25:04 -0300

    Separate files, test file created

--- phpirt.php
+++ phpirt.php
@@ -61,10 +61,4 @@ class SubscriptionProcessor{
 	}
 }
 
-$irt = new InstagramRealTime('699495b3bfaf4632bdc5096e7544ff23', '68af47b6e9174f5dbb94ef913fdc42b3', 'http://public.olapic.com/~aseba/PHPIRT/callback.php');
-$irt->addSubscription('tag', 'media', 'nyc');
-// $irt->addSubscription('tag', 'media', 'hq');
-// $irt->addSubscription('tag', 'media', 'catan');
-// $irt->deleteSubscription('all');
-
 ?>
