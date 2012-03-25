b895e35b374525e715522dcaa602b487e4ebaae0
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-24 23:11:43 -0300

    Starting to have something

--- callback.php
+++ callback.php
@@ -1,10 +1,11 @@
 <?php
 
 if(isset($_REQUEST['hub_challenge'])) echo $_GET['hub_challenge'];
-
-$file = file_get_contents('/tmp/updates.instagram');
-$file .= json_encode($_REQUEST);
-file_put_contents('/tmp/updates.instagram', $file);
-echo $file;
+else{
+	$file = file_get_contents('/tmp/updates.instagram');
+	$file .= json_encode($_REQUEST);
+	file_put_contents('/tmp/updates.instagram', $file);
+	echo $file;
+}
 
 ?>
--- phpirt.php
+++ phpirt.php
@@ -54,8 +54,10 @@ class InstagramRealTime {
 }
 
 $irt = new InstagramRealTime('699495b3bfaf4632bdc5096e7544ff23', '68af47b6e9174f5dbb94ef913fdc42b3', 'http://public.olapic.com/~aseba/PHPIRT/callback.php');
-// $irt->addSubscription('tag', 'media', 'apple');
-$irt->listSubscriptions();
+$irt->addSubscription('tag', 'media', 'nyc');
+$irt->addSubscription('tag', 'media', 'hq');
+$irt->addSubscription('tag', 'media', 'catan');
+// $irt->listSubscriptions();
 // $irt->deleteSubscription('all');
 
 ?>
