18450b75ae44766052dfd426d9cc8d5284bc2455
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-24 23:23:43 -0300

    Starting to have something

--- callback.php
+++ callback.php
@@ -4,7 +4,7 @@ if(isset($_GET['hub_challenge'])) echo $_GET['hub_challenge'];
 if(isset($_POST)){
 	$file = file_get_contents('/tmp/updates.instagram');
 	$data = print_r($_POST);
-	$fulldata = $file . "\n\n" . $data
+	$fulldata = $file . "\n\n" . $data;
 	file_put_contents('/tmp/updates.instagram', $dulldata);
 	echo $file;
 }
