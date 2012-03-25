abae92ee65bfbd52b7baa8727af58587b495ed5d
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-25 15:27:41 -0300

    Fixed test file

--- test.php
+++ test.php
@@ -2,11 +2,11 @@
 
 require 'phpirt.php';
 
-$irt = new InstagramRealTime('699495b3bfaf4632bdc5096e7544ff23', '68af47b6e9174f5dbb94ef913fdc42b3', 'http://public.olapic.com/~aseba/PHPIRT/callback.php');
-$irt->addSubscription('tag', 'media', 'nyc');
+$irt = new InstagramRealTime('Client ID', 'Client Secret', 'Accesible callback url');
+// $irt->addSubscription('tag', 'media', 'nyc');
 // $irt->addSubscription('tag', 'media', 'hq');
 // $irt->addSubscription('tag', 'media', 'catan');
+// $irt->listSubscriptions();
 // $irt->deleteSubscription('all');
 
-?>
 ?>
