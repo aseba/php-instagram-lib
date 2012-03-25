29277dfead07c70a67aa51ef5e98a1a4a27f87c8
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-24 21:47:47 -0300

    Starting to have something

--- phpirt.php
+++ phpirt.php
@@ -13,18 +13,18 @@ class InstagramRealTime {
 		);
 	}
 
-	public function addSubscription(){
+	public function addSubscription($object, $aspect, $object_id=null){
 		$params = array_merge($this->settings, array(
-			'object' => 'user',
-			'aspect' => 'media'
+			'object' => $object,
+			'aspect' => $aspect
 		));
-
+		if(!is_null($object_id)) $params['object_id' => $object_id];
 		$curl = new Curl;
 		echo $curl->post('https://api.instagram.com/v1/subscriptions/', $params);
 	}
 }
 
 $irt = new InstagramRealTime('699495b3bfaf4632bdc5096e7544ff23', '68af47b6e9174f5dbb94ef913fdc42b3', 'http://public.olapic.com/~aseba/PHPIRT/callback.php');
-$irt->addSubscription();
+$irt->addSubscription('tag', 'media', 'apple');
 
 ?>
