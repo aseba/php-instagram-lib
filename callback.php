8d5a248eacd39bb1e3d60a0315ea8f61d48931c1
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-24 21:43:25 -0300

    Starting to have something

--- callback.php
+++ callback.php
@@ -4,4 +4,10 @@ if(isset($_GET)){
 	if(isset($_GET['hub_challenge'])) echo $_GET['hub_challenge'];
 }
 
+if(isset($_POST)){
+	$file = file_get_contents('updates.instagram');
+	$file .= json_encode($_POST);
+	file_put_contents('updates.instagram', $file);
+}
+
 ?>
