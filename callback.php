5dba967f319cdc62ccc11114b4ce7466e9efddb6
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2012-03-24 23:09:12 -0300

    Starting to have something

--- callback.php
+++ callback.php
@@ -1,13 +1,9 @@
 <?php
 
-if(isset($_GET)){
-	if(isset($_GET['hub_challenge'])) echo $_GET['hub_challenge'];
-}
+if(isset($_REQUEST['hub_challenge'])) echo $_GET['hub_challenge'];
 
-if(isset($_POST)){
-	$file = file_get_contents('updates.instagram');
-	$file .= json_encode($_POST);
-	file_put_contents('updates.instagram', $file);
-}
+$file = file_get_contents('updates.instagram');
+$file .= json_encode($_REQUEST);
+file_put_contents('updates.instagram', $file);
 
 ?>
--- phpirt.php
+++ phpirt.php
@@ -4,6 +4,7 @@ require 'curl/curl.php';
 
 class InstagramRealTime {
 	private $settings = array();
+	private $base_url = 'https://api.instagram.com/v1';
 
 	public function InstagramRealTime($client_id, $client_secret, $callback_url){
 		$this->settings = array(
@@ -20,11 +21,41 @@ class InstagramRealTime {
 		));
 		if(!is_null($object_id)) $params['object_id'] = $object_id;
 		$curl = new Curl;
-		echo $curl->post('https://api.instagram.com/v1/subscriptions/', $params);
+		$url = $this->base_url . '/subscriptions/';
+		echo $curl->post($url, $params);
+	}
+
+	public function listSubscriptions(){
+		$params = $this->settings;
+		$curl = new Curl;
+		$url = $this->base_url . '/subscriptions/';
+		print_r(json_decode($curl->get($url, $params), true));
+	}
+
+	public function deleteSubscription($object=null, $id=null){
+		if(is_null($object) and is_null($id)) throw new Exception("You must set and object type or and object id. If you want to delete all set object as 'all'");
+		elseif(!is_null($object) and is_null($id)) {
+			$params = array_merge($this->settings, array(
+				'object' => $object
+			));
+		}
+		elseif(is_null($object) and !is_null($id)) {
+			$params = array_merge($this->settings, array(
+				'id' => $id
+			));
+		}
+		else throw new Exception("You must set and object type or and object id, not both"); 
+		$curl = new Curl;
+		$curl->debug = true;
+		$url = $this->base_url . '/subscriptions/';
+		print_r(json_decode($curl->delete($url, $params), true));
+		$this->listSubscriptions();
 	}
 }
 
 $irt = new InstagramRealTime('699495b3bfaf4632bdc5096e7544ff23', '68af47b6e9174f5dbb94ef913fdc42b3', 'http://public.olapic.com/~aseba/PHPIRT/callback.php');
-$irt->addSubscription('tag', 'media', 'apple');
+// $irt->addSubscription('tag', 'media', 'apple');
+$irt->listSubscriptions();
+// $irt->deleteSubscription('all');
 
 ?>
