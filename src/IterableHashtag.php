b7e99c320f79c76fb35e8a54b8201d8afd35be97
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2017-04-16 00:00:07 -0300

    Better pagination implementation

--- src/IterableHashtag.php
+++ src/IterableHashtag.php
@@ -4,53 +4,56 @@ namespace aseba\Instagram;
 
 class IterableHashtag implements \Iterator
 {
+    private $repository;
+    private $hashtag;
+    private $pagination;
+    private $content;
+    private $position;
+
     public function __construct(InstagramRepository $repository, $hashtag)
     {
-        $this->repository = $repository;
-        $this->hashtag = $hashtag;
+      $this->repository = $repository;
+      $this->hashtag = $hashtag;
 
-        $this->pagination = null;
-        $this->content = null;
+      $this->pagination = [];
+      $this->content = [];
 
-        $this->max_tag_id = null;
+      $this->position = 0;
     }
 
     public function rewind()
     {
+      $this->position = 0;
     }
 
     public function valid()
     {
-        if (is_null($this->pagination)) {
-            return true;
-        } else {
-            return !(is_null($this->max_tag_id));
-        }
+      return
+        !is_null($this->pagination[$this->position-1]->next_max_tag_id)
+        or $this->position == 0;
     }
 
     public function next()
     {
-        if (!is_null($this->pagination) && isset($this->pagination->next_max_tag_id)) {
-            $this->max_tag_id = $this->pagination->next_max_tag_id;
-        } else {
-            $this->max_tag_id = null;
-        }
+      ++$this->position;
     }
 
     public function current()
     {
-        $content = $this->repository->getInstagramsFromHashtag($this->hashtag, $this->max_tag_id);
-        $this->pagination = $this->repository->getPaginationInfo();
-        $this->content = $content;
+      if(!isset($this->content[$this->position])) {
+        $content = $this->repository->getInstagramsFromHashtag(
+          $this->hashtag,
+          $this->pagination[$this->position-1]->next_max_tag_id
+        );
+        $this->pagination[$this->position] = $this->repository->getPaginationInfo();
+        $this->content[$this->position] = $content;
+      }
 
-        return $this->content;
+      return $this->content[$this->position];
     }
 
     public function key()
     {
-        return [
-        'max_tag_id' => $this->max_tag_id,
-        'min_tag_id' => $this->min_tag_id,
-      ];
+      return $this->position;
     }
 }
